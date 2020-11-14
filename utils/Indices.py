355c47b95a7f8c2a30611c1aa1d958e0012df28b
miltonluaces <miltonluacs@gmail.com>
2020-11-14 22:12:46 +0000

    SP500 sector lists

--- utils/Indices.py
+++ utils/Indices.py
@@ -4,17 +4,37 @@ import investpy
 class Indices:
 
     def __init__(self):
+        self.path = 'D:/Invest/Data/stock_lists/sp500_sectors/'
         self.Idx = pd.read_csv('D:/Invest/data/indices.csv', encoding = 'ISO-8859-1')
+        self.sectors = ['CD', 'CS', 'HC', 'I', 'IT','M', 'RE', 'TS', 'U', 'F', 'E']
 
     def get_hist(self, index, start, end, interval):
         idex = self.Idx[self.Idx['Index']==index]['Name'].values[0]
         return investpy.get_index_historical_data(index=idex, country='united states', from_date=start, to_date=end, interval=interval)
 
+    def get_sp500_sectors(self):
+        dfs = []
+        for sector in self.sectors:
+            df = pd.read_csv(self.path + 'sp500' + sector + '.csv')
+            df['Sector'] = sector
+            dfs.append(df)
+
+        sps = pd.concat(dfs)
+        sps.index = sps.Symbol
+        sps.to_csv(self.path + 'sp500_sectors.csv')
+        return sps
+        
 
 
 if __name__ == '__main__':
 
     idx = Indices()
-    df = idx.get_hist(index='sp500re', start='01/01/2020', end='01/11/2020', interval='Daily')
-    print(df.shape[0])
-    print(df.head())
+    #df = idx.get_hist(index='sp500re', start='01/01/2020', end='01/11/2020', interval='Daily')
+    #print(df.shape[0])
+    #print(df.head())
+
+    idx.sectors = ['CD', 'TS']
+    sps = idx.get_sp500_sectors()
+    print(sps)
+
+    print(sps[sps['Symbol']=='AMZN']['Sector'])
